f5f26ff8857bedbf83a9fedd1c483806dc75538b
bdSentz <sentzben98@yahoo.com>
2018-03-07 17:02:54 -0500

    Added classes that I forgot to stage

---
+++ src/edu/ycp/cs320/IslandAdventure/controller/InventoryController.java
@@ -0,0 +1,23 @@
+package edu.ycp.cs320.IslandAdventure.controller;
+
+import edu.ycp.cs320.IslandAdventure.model.Inventory;
+
+public class InventoryController 
+{
+	private Inventory model;
+	
+	public void setModel(Inventory model) 
+	{
+		this.model = model;
+	}
+	
+	public void createGame()
+	{
+		model.setWoodAmount(0);
+	}
+	
+	public void changeWoodAmount(double amount)
+	{
+		model.setWoodAmount(model.getWoodAmount() + amount);
+	}
+}
