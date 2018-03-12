3d58608c9f6dffec28046abacdaf26084fa0f7b8
Nolan Hamilton <nhamilton@ycp.edu>
2018-03-12 17:36:47 -0400

    Nolan-Hamilton: Created NewAccountServlet and newAccount.jsp

--- src/edu/ycp/cs320/IslandAdventure/servlet/NewAccountServlet.java
+++ src/edu/ycp/cs320/IslandAdventure/servlet/NewAccountServlet.java
@@ -11,35 +11,30 @@ import edu.ycp.cs320.IslandAdventure.controller.ActionController;
 import edu.ycp.cs320.IslandAdventure.controller.InventoryController;
 import edu.ycp.cs320.IslandAdventure.model.Inventory;
 
-public class LoginServlet extends HttpServlet {
+public class NewAccountServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		System.out.println("Login Servlet: doGet");
+		System.out.println("New Account Servlet: doGet");
 		
-		req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
+		req.getRequestDispatcher("/_view/newAccount.jsp").forward(req, resp);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		System.out.println("LoginServlet: doPost");
+		System.out.println("NewAccountServlet doPost");
 		
 		// Navigating to new page by pressing button
-		if (req.getParameter("justPlay") != null) {
-			System.out.println("justPlay has been clicked!");
-			req.getRequestDispatcher("/_view/index.jsp").forward(req, resp); //Go to this page
-		}else{
-			// if create new account was selected, then go to account page
-			if (req.getParameter("newAcc") != null){
-				System.out.println("newAcc has been clicked");
-				req.getRequestDispatcher("/_view/newAccount.jsp").forward(req, resp); //Go to this page
-			}
+		if (req.getParameter("back2Login") != null){
+			System.out.println("back2Login has been clicked");
+			req.getRequestDispatcher("/_view/login.jsp").forward(req, resp); //Go to this page
 		}
+		
 		/* THIS IS JUST COPIED FROM INDEXSERVLET
 		// create Inventory model - model does not persist between requests
 		// must recreate it each time a Post comes in 
@@ -65,7 +60,7 @@ public class LoginServlet extends HttpServlet {
 		controller.interpretAction(action);
 		
 		// Forward to view to render the result HTML document
-		req.getRequestDispatcher("/_view/login.jsp").forward(req, resp);
+		req.getRequestDispatcher("/_view/newAccount.jsp").forward(req, resp);
 		*/
 	}
 }
--- war/_view/newAccount.jsp
+++ war/_view/newAccount.jsp
@@ -4,7 +4,7 @@
 
 <html>
 	<head>
-		<title>Island Adventure - Login</title>
+		<title>Island Adventure - New Account</title>
 		<link href="<c:url value="/resources/css/main.css" />" rel="stylesheet">
 		<!-- Enter this into URL: http://localhost:8081/IslandAdventure/login -->
 	</head>
@@ -15,27 +15,26 @@
 		</div>
 		
 		<div id= "titleText">
-			<h2>Login Page</h2>
+			<h2>New Account</h2>
 		</div>
 		
 		<div id= "inputBox">
 			
-			<form action="${pageContext.servletContext.contextPath}/login" method="post">
+			<form action="${pageContext.servletContext.contextPath}/newAccount" method="post">
 				Username:<input type="text" name="action" size="20" value="${action}" />
 				<br><br>
 				Password:<input type="text" name="action" size="20" value="${action}" />
 				<br><br>
-				<input type="Submit" name="submit" value="Login!">
+				Confirm Password:<input type="text" name="action" size="20" value="${action}" />
+				<br><br>
+				<input type="Submit" name="submit" value="Create Account!">
 			</form>
 		</div>
 		
 		<div id= "inputBox">
-			<form action="${pageContext.servletContext.contextPath}/login" method="post">
-				<input type="Submit" name="newAcc" value="Create New Account">	
-				<input type="Submit" name="justPlay" value="Just Start Playing">
+			<form action="${pageContext.servletContext.contextPath}/newAccount" method="post">
+				<input type="Submit" name="back2Login" value="Back to Login">	
 			</form>
-			<p>Warning:
-			You will not be able to save if you do not login with an account!</p>
 		</div>
 		
 		
